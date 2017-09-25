f85c2b914fa144688f4222d4d7e80da82eddc7ea
Victor <victor@mexiton.com>
2017-09-25 09:59:19 +0800

    Add POST, body parser and middleware

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const os = require("os");
 var app = require("express")();
 var tester = require("./dynamo-test");
+var parser = require("body-parser");
+
 // tester.init();
-tester.append({
-    name: "user1",
-    created: new Date().getDate().toISOString()
-});
+// tester.append({
+//     name: "user1",
+//     created: new Date().getDate().toISOString()
+// });
+
+// configure middleware for express
+app.use(parser.json())
+app.use(parser.urlencoded({ extended: true }));
 
 app.get("/", function (request, response) {
     response.send("Listening on http://" + os.hostname() + ":" + listener.address().port);
+});
+
+app.post("/update", function (request, response) {
+    response.send(JSON.stringify({
+        key: request.body.key || null,
+        value: request.body.value || null
+    }));
 });
 
 app.get("/testrun", function (request, response) {
