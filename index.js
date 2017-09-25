c8503e881ad15ba1212820db6ef0d8e87d0563a9
Victor <victor@mexiton.com>
2017-09-25 10:28:50 +0800

    Compliance to ESLint

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-const os = require("os");
-var app = require("express")();
-var tester = require("./dynamo-test");
-var parser = require("body-parser");
+const os = require('os');
+var app = require('express')();
+var tester = require('./dynamo-test');
+var parser = require('body-parser');
 
 // tester.init();
 // tester.append({
-//     name: "user1",
+//     name: 'user1',
 //     created: new Date().getDate().toISOString()
 // });
 
 // configure middleware for express
-app.use(parser.json())
+app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
-app.get("/", function (request, response) {
-    response.send("Listening on http://" + os.hostname() + ":" + listener.address().port);
+app.get('/', function (request, response) {
+  response.send('Listening on http://' + os.hostname() + ':' + listener.address().port);
 });
 
-app.post("/update", function (request, response) {
-    response.send(JSON.stringify({
-        key: request.body.key || null,
-        value: request.body.value || null
-    }));
+app.post('/update', function (request, response) {
+  response.send(JSON.stringify({
+    key: request.body.key || null,
+    value: request.body.value || null
+  }));
 });
 
-app.get("/testrun", function (request, response) {
-    response.send(tester.run());
+app.get('/testrun', function (request, response) {
+  response.send(tester.run());
 });
 
 var listener = app.listen(3000, function () {
-    console.log("http://" + os.hostname() + ":" + listener.address().port);
+  console.log('http://' + os.hostname() + ':' + listener.address().port);
 })
-
