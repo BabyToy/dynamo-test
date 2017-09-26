260bf69c0a350014e7100b6da013d873c87f6595
Victor <victor@mexiton.com>
2017-09-26 12:21:20 +0800

    Add update method and error handling

--- index.js
+++ index.js
@@ -3,24 +3,47 @@ var app = require('express')();
 var tester = require('./dynamo-test');
 var parser = require('body-parser');
 
-// tester.init();
+tester.init();
 // tester.append({
-//     name: 'user1',
-//     created: new Date().getDate().toISOString()
+//   name: 'user2',
+//   created: new Date().toISOString(),
+//   characterlevel: 0
 // });
 
 // configure middleware for express
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
+app.use(function (error, request, response, next) {
+  if (error) {
+    response.status(200).send();
+  } else {
+    response.status(201);
+    next();
+  }
+});
+
+console.log(new Date().toISOString());
 
 app.get('/', function (request, response) {
   response.send('Listening on http://' + os.hostname() + ':' + listener.address().port);
 });
 
+app.post('/add', function (request, response) {
+  tester.append({
+    username: request.body.username,
+    characterlevel: request.body.characterlevel
+  });
+  response.status(200).send();
+});
+
 app.post('/update', function (request, response) {
+  tester.update({
+    id: request.body.id,
+    characterlevel: request.body.characterlevel
+  });
   response.send(JSON.stringify({
-    key: request.body.key || null,
-    value: request.body.value || null
+    id: request.body.id || null,
+    characterlevel: request.body.characterlevel || null
   }));
 });
 
